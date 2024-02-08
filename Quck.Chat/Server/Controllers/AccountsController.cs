84c76a26fb26022ea6b1eec7c259ae9919883f76
tahminasqa <tahminasqa@gmail.com>
2024-02-08 00:05:07 -0500

    * Added accounts controller base

---
+++ Quck.Chat/Server/Controllers/AccountsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quck.Chat.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+    }
+}
